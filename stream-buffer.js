c61bbbc70cf7d6600d44c9423a71f66822fea497
Shuvro Dev Mondal <shuvrodevmondal337@gmail.com>
2024-12-03 13:46:51 -0800

    fixed path issues & error handling

--- stream-buffer.js
+++ stream-buffer.js
@@ -6,17 +6,34 @@ const fs = require('fs')
 
   // server listener
   server.on('request', (req, res)=>{
-    if(req.url === '/read-file', req.method === 'GET'){
-    // streamig file reading
-    const readableStream =  fs.createReadStream(process.cwd() +'/text/large-text.txt') //or __dirname + path
 
+
+    if (req.url === '/' && req.method === 'GET') {
+        // Handle root route
+        res.statusCode=200
+        res.end('Welcome to the raw Node.js server!');
+      } else if(req.url === '/read-file' &&  req.method === 'GET'){
+    // streamig file reading
+          const readableStream =  fs.createReadStream(process.cwd() +'/text/large-text.txt') //or __dirname + path
+    // listeners
     readableStream.on('data',(buffer)=>{
+        res.statusCode=200
         res.write(buffer)
     })
-    readableStream.on('end',(buffer)=>{
+    readableStream.on('end',()=>{
+        res.statusCode=200
         res.end('Hello from raw node.js server')
     })
-    };
+    readableStream.on('error',(error)=>{
+        console.log(error);
+        res.statusCode=500
+        res.end('something went wrong')
+    })
+    }else {
+        // Handle other routes or invalid paths
+        res.statusCode = 404;
+        res.end('Not Found');
+      }
   })
 
 server.listen(5000,()=>{
